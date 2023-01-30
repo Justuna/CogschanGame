d4b20271594b1ce72e95c645e4fa5f78a992f274
Justuna <jlpisa999@yahoo.com>
2023-01-29 22:48:50 -0500

    basic enemy spawn system working

--- Cogschan Project/Assets/Scripts/Enemies/Spawning/EnemySpread.cs
+++ Cogschan Project/Assets/Scripts/Enemies/Spawning/EnemySpread.cs
@@ -11,13 +11,14 @@ public class EnemySpread : ScriptableObject
     public float StartDifficulty;
     public float MaxDifficulty;
     public float Noise;
+    public int MinCount;
     public int MaxCount;
 
     public int EvaluateEnemyCount(float difficulty)
     {
         float noise = Random.Range(-1.0f, 1.0f) * Noise;
         float t = Mathf.Clamp((difficulty - StartDifficulty) / (MaxDifficulty - StartDifficulty) + noise, 0, 1.0f);
-        int n = Mathf.CeilToInt(DifficultyRamp.Evaluate(t) * MaxCount);
+        int n = MinCount + Mathf.CeilToInt(DifficultyRamp.Evaluate(t) * (MaxCount - MinCount));
         return n;
     }
 }
--- Cogschan Project/Assets/Scripts/Enemies/Spawning/SpawnController.cs
+++ Cogschan Project/Assets/Scripts/Enemies/Spawning/SpawnController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SpawnController : MonoBehaviour
 {
+    [Header("Difficulty Variables")]
     public float DifficultyScale = 1.0f;
     public float StartingDifficulty = 0.0f;
     public float MaxDifficulty = 100.0f;
@@ -13,6 +15,16 @@ public class SpawnController : MonoBehaviour
 
     public float ExtraSpawnTimerEarliest = 25.0f;
     public float ExtraSpawnTimerLatest = 50.0f;
+
+    [Header("Spawning Variables")]
+    public EnemyPatternGroup MainWaveGroup;
+    public Transform Player;
+    public float MinDistance = 10;
+    public float MaxDistance = 20;
+    public float MinSpread = 0.5f;
+    public float MaxNavmeshRaycastDistance = 20;
+    public int MaxAttempts = 100;
+    public float Lopsidedness = 2;
 
     private float _difficulty;
     private float _mainSpawnTimer;
@@ -61,6 +73,45 @@ public class SpawnController : MonoBehaviour
     void SpawnMainWave()
     {
         Debug.Log("Spawning Main Wave...");
+
+        Dictionary<GameObject, int> dict = MainWaveGroup.PickPattern(_difficulty);
+        List<Vector3> spawns = new List<Vector3>();
+
+        foreach (KeyValuePair<GameObject, int> kvp in dict)
+        {
+            Debug.Log("At a difficulty of " + _difficulty + ", spawning " + kvp.Value + " " + kvp.Key.name + "(s).");
+
+            for (int i = 0; i < kvp.Value; i++)
+            {
+                //Distance is random, but weighted to be closer
+                NavMeshHit hit = new NavMeshHit();
+                for (int j = 0; j < MaxAttempts; j++)
+                {
+                    bool legal = true;
+
+                    float dist = Mathf.Pow(Random.Range(0, 1.0f), Lopsidedness) * MaxDistance + MinDistance;
+                    float angle = Random.Range(0, 360.0f);
+
+                    Vector3 displacement = Quaternion.Euler(0, angle, 0) * Vector3.forward * dist;
+
+                    NavMesh.SamplePosition(Player.position + displacement, out hit, MaxNavmeshRaycastDistance, NavMesh.AllAreas);
+                    if (hit.position == null) continue;
+                    foreach (Vector3 point in spawns)
+                    {
+                        if (Vector3.Distance(point, hit.position) < MinSpread)
+                        {
+                            legal = false;
+                            break;
+                        }
+                    }
+
+                    if (legal) break;
+                }
+                spawns.Add(hit.position);
+
+                Instantiate(kvp.Key, hit.position, Quaternion.identity);
+            }
+        }
     }
 
     void SpawnExtraWave()
