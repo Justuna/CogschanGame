acc87b5a72c76181c153d46f99bf8f89844ce2af
DoDaWu <102868333+noahwu8888@users.noreply.github.com>
2023-02-10 17:56:37 -0500

    Attempting Graph Checkpoint, do not merge

--- Cogschan Project/Assets/Scripts/Entities/AI/JanglingAI.cs
+++ Cogschan Project/Assets/Scripts/Entities/AI/JanglingAI.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class JanglingAI : MonoBehaviour
 {
     public Transform Model;
-    public Transform[] posNodes;
+    public GameObject[] posNodes;
+    public bool[][] adjMatrix;
     public bool shouldMove;
     public Transform destination;
     public Transform Player;
@@ -13,7 +14,7 @@ public class JanglingAI : MonoBehaviour
     private void Awake()
     {
         shouldMove = false;
-        destination = posNodes[1];
+        destination = posNodes[1].transform;
     }
 
     // Update is called once per frame
@@ -21,12 +22,18 @@ public class JanglingAI : MonoBehaviour
     {
         if (shouldMove)
         {
-            lookAtPoint(posNodes[1]);
+            //Looks at and moves to destination Node
+            Model.LookAt(posNodes[1].transform);
             transform.position = Vector3.MoveTowards(transform.position, destination.position, speed * Time.deltaTime);
+            //Once reached, set shouldMove to false
+            if(transform.position.Equals(destination)){
+                shouldMove = false;
+            }
         }
     }
-    private void lookAtPoint(Transform destination)
-    {
-        Model.LookAt(destination.position);
+
+
+    private void PickNewNode(){
+        
     }
 }
