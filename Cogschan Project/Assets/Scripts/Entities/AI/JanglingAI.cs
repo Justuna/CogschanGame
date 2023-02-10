cb655409c2495e48da5b961d9a9bfba681985e96
DoDaWu <102868333+noahwu8888@users.noreply.github.com>
2023-02-10 16:01:47 -0500

    Learning GitKraken

---
+++ Cogschan Project/Assets/Scripts/Entities/AI/JanglingAI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JanglingAI : MonoBehaviour
+{
+    public Transform[][] posNodes;
+    
+    
+    
+    public float speed;
+    private void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
