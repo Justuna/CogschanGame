cfe79003b45b680bcf6d74ed7c5b4d01c4c6e750
DoDaWu <102868333+noahwu8888@users.noreply.github.com>
2023-02-10 17:18:02 -0500

    The Box Moves

--- Cogschan Project/Assets/Scripts/Entities/AI/JanglingAI.cs
+++ Cogschan Project/Assets/Scripts/Entities/AI/JanglingAI.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class JanglingAI : MonoBehaviour
 {
-    public Transform[][] posNodes;
-    
-    
-    
+    public Transform Model;
+    public Transform[] posNodes;
+    public bool shouldMove;
+    public Transform destination;
+    public Transform Player;
     public float speed;
     private void Awake()
     {
-        
+        shouldMove = false;
+        destination = posNodes[1];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (shouldMove)
+        {
+            lookAtPoint(posNodes[1]);
+            transform.position = Vector3.MoveTowards(transform.position, destination.position, speed * Time.deltaTime);
+        }
+    }
+    private void lookAtPoint(Transform destination)
+    {
+        Model.LookAt(destination.position);
     }
 }
