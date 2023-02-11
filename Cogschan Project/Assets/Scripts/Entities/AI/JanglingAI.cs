2b69b1a244d102509caf91bc0b7368958efebe4c
DoDaWu <102868333+noahwu8888@users.noreply.github.com>
2023-02-10 22:11:36 -0500

    add comments

--- Cogschan Project/Assets/Scripts/Entities/AI/JanglingAI.cs
+++ Cogschan Project/Assets/Scripts/Entities/AI/JanglingAI.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class JanglingAI : MonoBehaviour
 {
+    //For making model look away
     public Transform Model;
+    //List of predetermined destination Nodes
     public GameObject[] destNodes;
+    //Whether or not Jangling should be moving
     public bool shouldMove;
+    //the node number its currently on
     public int currentNode;
+    //the node number of its destination
     public int nextNode;
+    //the position of its destination
     public Transform destination;
+    //reference to the player character
     public Transform Player;
     public float speed;
+    public float runDistance;
+    public float noticeDistance;
     private void Awake()
     {
         shouldMove = false;
+        //resets Jangling position to first node in list
         transform.position = destNodes[0].transform.position;
         currentNode = 0;
     }
@@ -28,23 +38,31 @@ public class JanglingAI : MonoBehaviour
             Model.LookAt(destination.transform);
             transform.position = Vector3.MoveTowards(transform.position, destination.position, speed * Time.deltaTime);
             //Once reached, set shouldMove to false
+            //reset destination and set current node ot next
             if (transform.position.Equals(destination.position) && shouldMove)
             {
                 shouldMove = false;
                 destination = null;
                 currentNode = nextNode;
-                Model.LookAt(Player.transform);
             }
         }
         else
         {
-            if(Vector3.Distance(transform.position, Player.position) < 4){
+            //checks if player is close enough to initiate run away
+            if (Vector3.Distance(transform.position, Player.position) < runDistance)
+            {
                 PickNewDestination();
                 shouldMove = true;
-            };
-            Model.LookAt(Player.transform);
+            }
+            //checks if player is close enough for jangly  to notice
+            else if (Vector3.Distance(transform.position, Player.position) < noticeDistance)
+            {
+                Model.LookAt(Player.transform);
+            }
         }
     }
+
+    //Picks node from adjacents furthest away from player
     private void PickNewDestination()
     {
         JanglingNode node = destNodes[currentNode].GetComponent<JanglingNode>();
@@ -60,7 +78,5 @@ public class JanglingAI : MonoBehaviour
         }
         destination = destNodes[node.adjacentNodes[bestNode]].transform;
         nextNode = node.adjacentNodes[bestNode];
-        Debug.Log(destination + " " + nextNode);
-
     }
 }
