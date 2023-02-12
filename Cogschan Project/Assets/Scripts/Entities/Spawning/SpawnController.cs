49b625e2d8917d2371b18f99d5809997a443cd3d
Justuna <jlpisa999@yahoo.com>
2023-02-12 16:57:28 -0500

    added auxiliary wave logic

--- Cogschan Project/Assets/Scripts/Entities/Spawning/SpawnController.cs
+++ Cogschan Project/Assets/Scripts/Entities/Spawning/SpawnController.cs
@@ -13,18 +13,29 @@ public class SpawnController : MonoBehaviour
     public float MainSpawnTimerEarliest = 20.0f;
     public float MainSpawnTimerLatest = 30.0f;
 
-    public float ExtraSpawnTimerEarliest = 25.0f;
-    public float ExtraSpawnTimerLatest = 50.0f;
+    public float AuxSpawnTimerEarliest = 25.0f;
+    public float AuxSpawnTimerLatest = 50.0f;
 
-    [Header("Spawning Variables")]
-    public EnemyPatternGroup MainWaveGroup;
+    [Header("General Spawning Variables")]
     public Transform Player;
-    public float MinDistance = 10;
-    public float MaxDistance = 20;
-    public float MinSpread = 0.5f;
-    public float MaxNavmeshRaycastDistance = 20;
     public int MaxAttempts = 100;
+
+    [Header("Main Wave Variables")]
+    public EnemyPatternGroup MainWaveGroup;
+    public float MainWaveMinDistance = 10;
+    public float MainWaveMaxDistance = 20;
+    public float MainWaveMinSpread = 0.5f;
+    public float MainWaveMaxNavmeshRaycastDistance = 20;
     public float Lopsidedness = 2;
+
+    [Header("Auxiliary Wave Variables")]
+    public EnemyPatternGroup AuxiliaryGroup;
+    public float AuxWaveCenterMinDistance = 10;
+    public float AuxWaveCenterMaxDistance = 20;
+    public float AuxWaveMinDistance = 10;
+    public float AuxWaveMaxDistance = 20;
+    public float AuxWaveMinSpread = 0.5f;
+    public float AuxWaveMaxNavmeshRaycastDistance = 20;
 
     private float _difficulty;
     private float _mainSpawnTimer;
@@ -67,7 +78,7 @@ public class SpawnController : MonoBehaviour
 
     void SetExtraSpawnTimer()
     {
-        _extraSpawnTimer = Random.Range(ExtraSpawnTimerEarliest, ExtraSpawnTimerLatest);
+        _extraSpawnTimer = Random.Range(AuxSpawnTimerEarliest, AuxSpawnTimerLatest);
     }
 
     void SpawnMainWave()
@@ -89,16 +100,16 @@ public class SpawnController : MonoBehaviour
                 {
                     bool legal = true;
 
-                    float dist = Mathf.Pow(Random.Range(0, 1.0f), Lopsidedness) * MaxDistance + MinDistance;
+                    float dist = Mathf.Pow(Random.Range(0, 1.0f), Lopsidedness) * MainWaveMaxDistance + MainWaveMinDistance;
                     float angle = Random.Range(0, 360.0f);
 
                     Vector3 displacement = Quaternion.Euler(0, angle, 0) * Vector3.forward * dist;
 
-                    NavMesh.SamplePosition(Player.position + displacement, out hit, MaxNavmeshRaycastDistance, NavMesh.AllAreas);
+                    NavMesh.SamplePosition(Player.position + displacement, out hit, MainWaveMaxNavmeshRaycastDistance, NavMesh.AllAreas);
                     if (hit.position == null) continue;
                     foreach (Vector3 point in spawns)
                     {
-                        if (Vector3.Distance(point, hit.position) < MinSpread)
+                        if (Vector3.Distance(point, hit.position) < MainWaveMinSpread)
                         {
                             legal = false;
                             break;
@@ -117,5 +128,51 @@ public class SpawnController : MonoBehaviour
     void SpawnExtraWave()
     {
         Debug.Log("Spawning Extra Wave...");
+
+        Dictionary<GameObject, int> dict = AuxiliaryGroup.PickPattern(_difficulty);
+        List<Vector3> spawns = new List<Vector3>();
+
+        NavMeshHit centerHit = new NavMeshHit();
+        float centerDist = Random.Range(0, 1.0f) * AuxWaveCenterMaxDistance + AuxWaveCenterMinDistance;
+        float centerAngle = Random.Range(0, 360.0f);
+
+        NavMesh.SamplePosition(Quaternion.Euler(0, centerAngle, 0) * Vector3.forward * centerDist + Player.position, out centerHit, Mathf.Infinity, NavMesh.AllAreas); ;
+        Vector3 epicenter = centerHit.position;
+
+        foreach (KeyValuePair<GameObject, int> kvp in dict)
+        {
+            Debug.Log("At a difficulty of " + _difficulty + ", spawning " + kvp.Value + " " + kvp.Key.name + "(s).");
+
+            for (int i = 0; i < kvp.Value; i++)
+            {
+                //Distance is random, but weighted to be closer
+                NavMeshHit hit = new NavMeshHit();
+                for (int j = 0; j < MaxAttempts; j++)
+                {
+                    bool legal = true;
+
+                    float dist = Random.Range(0, 1.0f) * AuxWaveMaxDistance + AuxWaveMinDistance;
+                    float angle = Random.Range(0, 360.0f);
+
+                    Vector3 displacement = Quaternion.Euler(0, angle, 0) * Vector3.forward * dist;
+
+                    NavMesh.SamplePosition(epicenter + displacement, out hit, AuxWaveMaxNavmeshRaycastDistance, NavMesh.AllAreas);
+                    if (hit.position == null) continue;
+                    foreach (Vector3 point in spawns)
+                    {
+                        if (Vector3.Distance(point, hit.position) < AuxWaveMinSpread)
+                        {
+                            legal = false;
+                            break;
+                        }
+                    }
+
+                    if (legal) break;
+                }
+                spawns.Add(hit.position);
+
+                Instantiate(kvp.Key, hit.position, Quaternion.identity);
+            }
+        }
     }
 }
