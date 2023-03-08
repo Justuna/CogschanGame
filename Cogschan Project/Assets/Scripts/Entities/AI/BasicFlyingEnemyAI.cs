0d70321b1b87b71d42ce99f16c25402a2a5ac023
Justuna <jlpisa999@yahoo.com>
2023-03-07 20:56:00 -0500

    began process of trying to port basic ai to flying enemy

--- Cogschan Project/Assets/Scripts/Entities/AI/BasicFlyingEnemyAI.cs
+++ Cogschan Project/Assets/Scripts/Entities/AI/BasicFlyingEnemyAI.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyingAIFollow : MonoBehaviour
+public class BasicFlyingEnemyAI : MonoBehaviour
 {
     public float Speed;
     public float OrbitDistance;
     public float PreferredHeight;
 
     public Transform Target;
+    public LayerMask GroundMask;
 
     private Rigidbody _rb;
 
@@ -17,11 +18,17 @@ public class FlyingAIFollow : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    void Update()
     {
         if (Vector3.Distance(transform.position, Target.position) <= OrbitDistance) return;
 
         Vector3 dir = Vector3.Normalize(Target.position - transform.position);
-        _rb.AddForce(dir * Speed);
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, PreferredHeight, GroundMask))
+        {
+            dir.y = Mathf.Max(dir.y, 0);
+        }
+
+        _rb.velocity = dir * Speed;
     }
 }
