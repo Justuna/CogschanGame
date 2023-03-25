5efe3c32aeefa3bde727abd90e96fcb8e9026b83
Justuna <jlpisa999@yahoo.com>
2023-03-25 18:34:24 -0400

    in the middle of changes

--- Cogschan Project/Assets/Scripts/Entities/AI/BasicFlyingEnemyAI.cs
+++ Cogschan Project/Assets/Scripts/Entities/AI/BasicFlyingEnemyAI.cs
@@ -4,23 +4,82 @@ using UnityEngine;
 
 public class BasicFlyingEnemyAI : MonoBehaviour
 {
+    [Header("General Attributes")]
+    public Transform Player;
+    public Transform Model;
+
+    public LayerMask GroundMask, PlayerMask;
+
     public float Speed;
     public float OrbitDistance;
     public float PreferredHeight;
 
-    public Transform Target;
-    public LayerMask GroundMask;
+    public BasicEnemyState State { get; protected set; }
 
-    private Rigidbody _rb;
+    protected UnityEngine.AI.NavMeshAgent _agent;
 
-    private void Awake()
+    public float SightRange;
+    public float LOSInterruptThreshold;
+
+    protected float _LOSInterruptClock;
+
+    protected bool _canSee;
+
+    protected Rigidbody _rb;
+
+    protected void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
+        _agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        State = BasicEnemyState.Confused;
+        _rangedAttackTimer = Random.Range(MinTimeUntilRangedAttack, MaxTimeUntilRangedAttack);
     }
 
-    void Update()
+    protected void Update()
     {
-        if (Vector3.Distance(transform.position, Target.position) <= OrbitDistance) return;
+        Vector3 dir = Player.position - transform.position;
+        Ray ray = new Ray(transform.position, dir);
+        RaycastHit hit = new RaycastHit();
+        bool LOS = Physics.CheckSphere(transform.position, SightRange, PlayerMask) && !Physics.Raycast(ray, out hit, dir.magnitude, GroundMask);
+        Debug.DrawLine(transform.position, hit.point, Color.red);
+        if (LOS)
+        {
+            _LOSInterruptClock = LOSInterruptThreshold;
+            _canSee = true;
+        }
+        else
+        {
+            if (_LOSInterruptClock > 0)
+            {
+                _LOSInterruptClock -= Time.deltaTime;
+                _canSee = true;
+            }
+            else _canSee = false;
+        }
+
+        Debug.Log(State);
+        switch (State)
+        {
+            case BasicEnemyState.Confused:
+                LookAroundConfused();
+                break;
+            case BasicEnemyState.Patrol:
+                Patrolling();
+                break;
+            case BasicEnemyState.Alerted:
+                Alerted();
+                break;
+            case BasicEnemyState.Chase:
+                ChasePlayer();
+                break;
+            case BasicEnemyState.ChaseLastSeen:
+                ChaseLastSeen();
+                break;
+            case BasicEnemyState.Waiting:
+                Waiting();
+                break;
+        }
+    }
+        /*if (Vector3.Distance(transform.position, Target.position) <= OrbitDistance) return;
 
         Vector3 dir = Vector3.Normalize(Target.position - transform.position);
         Ray ray = new Ray(transform.position, Vector3.down);
@@ -29,6 +88,5 @@ public class BasicFlyingEnemyAI : MonoBehaviour
             dir.y = Mathf.Max(dir.y, 0);
         }
 
-        _rb.velocity = dir * Speed;
-    }
+        _rb.velocity = dir * Speed;*/
 }
