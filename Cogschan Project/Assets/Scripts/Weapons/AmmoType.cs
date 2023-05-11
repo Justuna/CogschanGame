dd90f9d6a17e7acecf618fcd34fece3413cae61f
Justuna <jlpisa999@yahoo.com>
2023-05-11 11:20:55 -0400

    blow it up and start over

--- Cogschan Project/Assets/Scripts/Weapons/AmmoType.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName ="Ammo Type")]
-public class AmmoType : ScriptableObject
-{
-    public string Identifier;
-}
