844c0a474e79e4699f0e90178fc4debf76a6a3cf
Justuna <jlpisa999@yahoo.com>
2023-05-25 18:29:13 -0400

    began laying the groundwork for hitbox/hurtbox/health stuff

---
+++ Cogschan/Assets/Scripts/Entities/HealthTracker.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script for keeping track of an entity's health.
+/// </summary>
+public class HealthTracker : MonoBehaviour
+{
+    [SerializeField] private int _maxHealth;
+
+    /// <summary>
+    /// An event that is triggered when this entity reaches 0 health.
+    /// </summary>
+    public CogschanSimpleEvent OnDeath;
+    /// <summary>
+    /// The current health of this entity.
+    /// </summary>
+    public int Health { get; private set; }
+
+    private void Start()
+    {
+        Health = _maxHealth;
+    }
+
+    /// <summary>
+    /// Sets the entity's health to a certain value.
+    /// </summary>
+    /// <param name="health">The value to set health to.</param>
+    public void SetHealth(int health)
+    {
+        Health = Mathf.Clamp(health, 0, _maxHealth);
+
+        if (Health == 0) OnDeath.Invoke();
+    }
+
+    /// <summary>
+    /// Damages the entity by a certain amount of health points.
+    /// </summary>
+    /// <param name="amount">The amount to decrease health by.</param>
+    public void Damage(int amount)
+    {
+        SetHealth(Health - amount);
+    }
+
+    /// <summary>
+    /// Heals the entity by a certain amount of health points.
+    /// </summary>
+    /// <param name="amount">The amount to increase health by.</param>
+    public void Heal(int amount)
+    {
+        SetHealth(Health + amount);
+    }
+}
--- Cogschan/Assets/Scripts/Entities/Hurtbox.cs
+++ Cogschan/Assets/Scripts/Entities/Hurtbox.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Hurtbox : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float _critMultiplier;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
         
     }
+}
+
+public abstract class Hitbox : MonoBehaviour
+{
+    [SerializeField] protected int _damage;
+    [SerializeField] protected bool _isHealing = false;
+    [SerializeField] protected bool _canCrit = false;
+    [SerializeField] protected bool _hasImpulse = false;
+
+    // Public accessors for serialized private fields.
+    // Basically, we want them to be publicly readable and changeable from the inspector,
+    // but we don't want other scripts to be able to edit their values.
+
+    /// <summary>
+    /// The damage that this hitbox deals. If <c>IsHealing</c> is <c>true</c>, then this is the amount of healing instead.
+    /// </summary>
+    public int Damage => _damage;
+    /// <summary>
+    /// Whether or not this hitbox heals instead of damaging.
+    /// </summary>
+    public bool IsHealing => _isHealing;
+    /// <summary>
+    /// Whether or not this hitbox can benefit from the crit multiplier of a hurtbox.
+    /// </summary>
+    public bool CanCrit => _canCrit;
+    /// <summary>
+    /// Whether or not this hitbox should impart an impulse upon the entity it hits.
+    /// </summary>
+    public bool HasImpulse => _hasImpulse;
+
+    /// <summary>
+    /// The impulse that this hitbox should impart.
+    /// </summary>
+    /// <returns></returns>
+    public abstract Vector3 GetImpulse();
 }
