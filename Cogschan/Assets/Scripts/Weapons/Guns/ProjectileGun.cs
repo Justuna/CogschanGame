670d60bbe025a8c40d0d4e38750207e161a16e52
averageJoe325 <ked103102@gmail.com>
2023-05-25 23:32:22 -0400

    i did the projectile thingy
    idk the diff between fire and fireaccurate so i just made fireaccurate call fire ill figure this out later byeeee

--- Cogschan/Assets/Scripts/Weapons/Guns/ProjectileGun.cs
+++ Cogschan/Assets/Scripts/Weapons/Guns/ProjectileGun.cs
@@ -6,15 +6,17 @@ public class ProjectileGun : Gun
 {
     [Header("Projectile Attributes")]
     [Tooltip("The prefab of the projectile that this gun shoots.")]
-    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Projectile _projectilePrefab;
 
     protected override void Fire(Vector3 targetPosition)
     {
-        Debug.Log("Fire!");
+        Projectile proj = Instantiate(_projectilePrefab, transform.position + targetPosition.normalized, Quaternion.identity);
+        proj.SetDirection(targetPosition);
     }
 
     protected override void FireAccurate(Vector3 targetPosition)
     {
-        Debug.Log("Fire, but accurately!");
+        // TODO: Make this do different stuff
+        Fire(targetPosition);
     }
 }
---
+++ Cogschan/Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Projectiles to be fired out of <see cref="ProjectileGun"/>s.
+/// </summary>
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The speed of the projectile")]
+    private float _speed;
+
+    /// <summary>
+    /// Makes the projectile move in the specified direction.
+    /// </summary>
+    public void SetDirection(Vector3 direction)
+    {
+        GetComponent<Rigidbody>().velocity = _speed * direction.normalized;
+    }
+}
+
