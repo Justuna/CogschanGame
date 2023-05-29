1dfe6ee058350e11b184e7b92479d3523c06972c
Justuna <jlpisa999@yahoo.com>
2023-05-29 11:31:29 -0400

    updated billboard comment

--- Cogschan/Assets/Scripts/UI/SimpleBillboard.cs
+++ Cogschan/Assets/Scripts/UI/SimpleBillboard.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-// A simple script responsible for making objects face the camera
-// Mostly useful for UI elements that sit in world space (eg. enemy healthbars), and therefore must be rotated to face the camera
+/// <summary>
+/// A script that makes the attached object face directly at the camera.
+/// </summary>
+/// <remarks>
+/// Useful for in-game UI, which otherwise might not face the camera and therefore be hard to read.
+/// </remarks>
 public class SimpleBillboard : MonoBehaviour
 {
     [SerializeField] private bool _isFlipped;
