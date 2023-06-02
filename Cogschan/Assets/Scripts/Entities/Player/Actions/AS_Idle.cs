8058c4a9f76bbff2bd4b1dd0f1643d53f3816963
spymanian <90417628+spymanian@users.noreply.github.com>
2023-06-01 20:31:43 -0400

    Connecting Reload Methods

--- Cogschan/Assets/Scripts/Entities/Player/Actions/AS_Idle.cs
+++ Cogschan/Assets/Scripts/Entities/Player/Actions/AS_Idle.cs
@@ -21,6 +21,10 @@ public class AS_Idle : MonoBehaviour, IActionState
         {
             IdleIntoFiring?.Invoke();
         }
+        else if (CogschanInputSingleton.Instance.IsReloadButtonDown && !_services.MovementController.IsSprinting)
+        {
+            IdleIntoReloading?.Invoke();
+        }
     }
 
     public void OnNextWeapon()
