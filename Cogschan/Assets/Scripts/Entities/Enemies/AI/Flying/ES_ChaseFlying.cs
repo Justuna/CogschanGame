60d5b319465839894b5dffd5be3219f8b9913f13
averageJoe325 <54290919+averageJoe325@users.noreply.github.com>
2023-06-15 14:38:23 -0400

    Update ES_ChaseFlying.cs
    ES_ChaseFlying class should be done but might also not be done. do not merge to main v2

--- Cogschan/Assets/Scripts/Entities/Enemies/AI/Flying/ES_ChaseFlying.cs
+++ Cogschan/Assets/Scripts/Entities/Enemies/AI/Flying/ES_ChaseFlying.cs
@@ -2,7 +2,27 @@ using UnityEngine;
 
 public class ES_ChaseFlying : MonoBehaviour, IEnemyState
 {
+    [SerializeField]
+    private EntityServiceLocator _services;
+    [SerializeField]
+    private float _attackRange;
+
+    public CogschanSimpleEvent LostPlayer;
+    public CogschanSimpleEvent InAttackRange;
+
+
     public void Behavior()
     {
+        if (_services.LOSChecker.CanSee)
+        {
+            Vector3 displacement = _services.LOSChecker.LastSeenPosition - transform.position;
+            if (displacement.magnitude > _attackRange)
+                _services.KinematicPhysics.DesiredVelocity =
+                    displacement.normalized * _services.FlyingAI.Speed;
+            else
+                InAttackRange();
+        }
+        else
+            LostPlayer();
     }
 }
