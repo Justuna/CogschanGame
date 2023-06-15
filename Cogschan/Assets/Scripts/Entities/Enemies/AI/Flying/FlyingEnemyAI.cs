a3c8daf52d428074e37856a64569b707c4393879
averageJoe325 <54290919+averageJoe325@users.noreply.github.com>
2023-06-15 14:51:52 -0400

    Update FlyingEnemyAI.cs
    State machine added. Trying to attack currently throws an exception as it appears there is no way out of the attack state.

--- Cogschan/Assets/Scripts/Entities/Enemies/AI/Flying/FlyingEnemyAI.cs
+++ Cogschan/Assets/Scripts/Entities/Enemies/AI/Flying/FlyingEnemyAI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public abstract class FlyingEnemyAI : MonoBehaviour
@@ -17,4 +18,73 @@ public abstract class FlyingEnemyAI : MonoBehaviour
     public float Speed => _speed;
 
     protected IEnemyState _state;
+
+    protected virtual void Awake()
+    {
+        es_Confused.LOSMade += ConfusedToAlerted;
+        es_Confused.Bored += ConfusedToPatrol;
+        es_Alerted.LOSBroken += AlertedToConfused;
+        es_Alerted.ReadyToChase += AlertedToChase;
+        es_Patrol.LOSMade += PatrolToAlerted;
+        es_Patrol.Bored += PatrolToConfused;
+        es_Chase.LostPlayer += ChaseToConfused;
+        es_Chase.InAttackRange += ChaseToAttack;
+    }
+
+    protected virtual void ConfusedToAlerted()
+    {
+        es_Alerted.ResetTimer();
+        _state = es_Alerted;
+    }
+
+    protected virtual void ConfusedToPatrol()
+    {
+        es_Patrol.Init();
+        _state = es_Patrol;
+    }
+
+    protected virtual void AlertedToConfused()
+    {
+        es_Confused.Init();
+        _state = es_Confused;
+    }
+
+    protected virtual void AlertedToChase()
+    {
+        _state = es_Chase;
+    }
+
+    protected virtual void PatrolToConfused()
+    {
+        es_Confused.Init();
+        _state = es_Confused;
+    }
+
+    protected virtual void PatrolToAlerted()
+    {
+        es_Alerted.ResetTimer();
+        _state = es_Alerted;
+    }
+
+    protected virtual void ChaseToConfused()
+    {
+        es_Confused.Init();
+        _state = es_Confused;
+    }
+
+#pragma warning disable CS0162 // Unreachable code detected
+    protected virtual void ChaseToAttack()
+    {
+        if (!_hasMeleeAttack && !_hasRangedAttack)
+            throw new InvalidOperationException("The enemy has no attacks and can not enter the attack state.");
+        throw new NotImplementedException("Entering the attack state would cause the enemy to get stuck, so lets not do that right now.");
+        _state = _hasMeleeAttack ? es_MeleeAttack : es_RangedAttack;
+    }
+#pragma warning restore CS0162 // Unreachable code detected
+
+    protected abstract void BeginMeleeAttack();
+    public abstract void EndMeleeAttack();
+
+    protected abstract void BeginRangedAttack();
+    public abstract void EndRangedAttack();
 }
