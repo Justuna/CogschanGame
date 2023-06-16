701b1171ae72a8db3025136325d6bfe8ec3cf3d9
averageJoe325 <54290919+averageJoe325@users.noreply.github.com>
2023-06-16 16:36:49 -0400

    fixed bug?

--- Cogschan/Assets/Scripts/Entities/Enemies/AI/Flying/FlyingKinematicPhysics.cs
+++ Cogschan/Assets/Scripts/Entities/Enemies/AI/Flying/FlyingKinematicPhysics.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem.WebGL;
 
 public class FlyingKinematicPhysics : KinematicPhysics
 {
@@ -7,7 +8,7 @@ public class FlyingKinematicPhysics : KinematicPhysics
     private float _acceleration;
     [SerializeField]
     [Tooltip("The maximum speed of the object under normal conditions. Must be positive.")]
-    private float _maxSpeed;
+    private float _maxSpeed; // TODO: The AI has  aspeed, what should i do with this?
     [SerializeField]
     [Tooltip("The multiplier applied to the velocity of the object at high speeds every second. Must be between 0 (no drag) and 1 (complete drag).")]
     [Range(0, 1)]
@@ -25,6 +26,7 @@ public class FlyingKinematicPhysics : KinematicPhysics
         if (_timer <= Time.deltaTime)
             return Vector3.right * 10 * _maxSpeed;
 #endif
+        _accelDir = (DesiredVelocity - _previousVelocity).normalized;
         Vector3 velocity = _previousVelocity;
         if (velocity.magnitude > _maxSpeed)
             velocity *= Mathf.Max(_maxSpeed / velocity.magnitude, Mathf.Pow(1 - _dragMultiplier, Time.deltaTime));
@@ -48,12 +50,11 @@ public class FlyingKinematicPhysics : KinematicPhysics
         return actualVelocity;
     }
 
+#if TESTING
     private void Update()
     {
-#if TESTING
         DesiredVelocity = Vector3.up * Mathf.Sin(_timer);
         _timer += Time.deltaTime;
-#endif
-        _accelDir = (DesiredVelocity - _previousVelocity).normalized;
     }
+#endif
 }
