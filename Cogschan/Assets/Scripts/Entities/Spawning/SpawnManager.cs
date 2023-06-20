351bfd830611a3f56bb402b5abfee6665a2deace
averageJoe325 <54290919+averageJoe325@users.noreply.github.com>
2023-06-20 13:49:27 -0400

    reintroduced functionality,
    spawnmanger fails in edge case, will fix

--- Cogschan/Assets/Scripts/Entities/Spawning/SpawnManager.cs
+++ Cogschan/Assets/Scripts/Entities/Spawning/SpawnManager.cs
@@ -1,7 +1,12 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-// The manager that handles spawning of all relevant objects.
+using static SpawnInfo;
+
+/// <summary>
+/// The manager that handles spawning of all relevant objects.
+/// </summary>
 public class SpawnManager : MonoBehaviour
 {
     /// <summary>
@@ -16,8 +21,11 @@ public class SpawnManager : MonoBehaviour
     [SerializeField]
     [Tooltip("The maximum amount of time between spawns. Must be at least the minimum spawn interval.")]
     private float _maxSpawnInterval;
+    [SerializeField]
+    [Tooltip("The weighting of the categories. Must contain at least one non-zero value and no negative values.")]
+    private float[] _catWeights;
 
-
+    [Header("Credit parameters")]
     [SerializeField]
     [Tooltip("The initial amount of credits. Must be nonnegative.")]
     private float _credits = 0;
@@ -28,11 +36,13 @@ public class SpawnManager : MonoBehaviour
     [Tooltip("The rate at which the rate at which credits are added to the Spawn Manager increases. Essentially the second derivative of credits. Must be nonnegative.")]
     private readonly float _creditAddRateRate;
 
+    private FiniteDistribution<SpawnCategory> _categoryDist;
     private float _spawnTimer = 0;
     private float _spawnInterval;
 
     private void Start()
     {
+        _categoryDist = new((SpawnCategory[])Enum.GetValues(typeof(SpawnCategory)), _catWeights);
         ResetSpawnInterval();
     }
 
@@ -52,40 +62,35 @@ public class SpawnManager : MonoBehaviour
     // Randomize the spawn interaval and reset the timer..
     private void ResetSpawnInterval()
     {
-        _spawnInterval = Random.Range(_minSpawnInterval, _maxSpawnInterval);
+        _spawnInterval = UnityEngine.Random.Range(_minSpawnInterval, _maxSpawnInterval);
         _spawnTimer = 0;
     }
 
     // Tries to spawn an object at the location of the manager.
     private bool Spawn()
     {
-        /*float totalWeight = 0;
-        List<(S, float)> boundaries = new();
-        foreach (SpawnCategory cat in Categories)
+        SpawnCategory category = _categoryDist.GetRandomValue();
+        List<SpawnInfo> spawnsInCat = new();
+        List<float> spawnWeights = new();
+        foreach (SpawnInfo spawn in Spawns)
         {
-            if (!cat.ContainsValidSpawns(_credits))
-                continue;
-            totalWeight += cat.Weight;
-            boundaries.Add((cat, totalWeight));
-        }
-        if (totalWeight == 0)
-            return false;
-        float rand = Random.Range(0, totalWeight);
-
-        SpawnCategory category = boundaries[-1].Item1;
-        foreach ((SpawnCategory, float) info in boundaries)
-        {
-            if (info.Item2 >= rand)
+            if (spawn.IsPurchasable(_credits) && spawn.Category == category)
             {
-                category = info.Item1;
-                break;
+                spawnsInCat.Add(spawn);
+                spawnWeights.Add(spawn.Weight);
             }
         }
-        SpawnInfo spawn = category.SelectSpawnInfo(_credits);
 
-        _credits -= spawn.Cost;
-        spawn.Spawner.Spawn(transform.position);
-        return true;*/
-        throw new System.NotImplementedException();
+        try
+        {
+            SpawnInfo selectedSpawn = new FiniteDistribution<SpawnInfo>(spawnsInCat, spawnWeights).GetRandomValue();
+            _credits -= selectedSpawn.Cost;
+            selectedSpawn.Spawner.Spawn(transform.position);
+            return true;
+        }
+        catch (ArgumentException)
+        {
+            return false;
+        }
     }
 }
