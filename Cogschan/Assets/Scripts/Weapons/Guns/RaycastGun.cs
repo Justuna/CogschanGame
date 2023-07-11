834535045c98302a453ac1175b8fbe0471464611
spymanian <90417628+spymanian@users.noreply.github.com>
2023-07-10 22:43:25 -0700

    Bug Fixed
    Player does not shoot when unpaused.

--- Cogschan/Assets/Scripts/Weapons/Guns/RaycastGun.cs
+++ Cogschan/Assets/Scripts/Weapons/Guns/RaycastGun.cs
@@ -17,17 +17,21 @@ public class RaycastGun : Gun
     private void FireRaycast(Vector3 targetPosition, SpreadEvent spread)
     {
         Vector3 dir = (targetPosition - _muzzle.transform.position).normalized;
-        for (int i = 0; i < _count; i++)
-        {
-            Vector3 spreadDir = spread is not null ? spread.ApplySpread(dir) : dir;
-            if (Physics.Raycast(_muzzle.position, spreadDir, out RaycastHit hitInfo))
-                HitObject(hitInfo.collider.gameObject);
-            if (_projectilePrefab is not null)
+
+        if(Time.timeScale != 0){
+            for (int i = 0; i < _count; i++)
             {
+                Vector3 spreadDir = spread is not null ? spread.ApplySpread(dir) : dir;
+                if (Physics.Raycast(_muzzle.position, spreadDir, out RaycastHit hitInfo))
+                    HitObject(hitInfo.collider.gameObject);
+                if (_projectilePrefab is not null)
+                {
                 Projectile proj = Instantiate(_projectilePrefab, _muzzle.position, Quaternion.identity);
                 proj.SetDirection(spreadDir);
+                }
             }
         }
+        
     }
 
     private void HitObject(GameObject gameObject)
