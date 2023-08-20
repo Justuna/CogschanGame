59de9608fd2d4f1a433db0dfa96822ac36a9fcfe
Justuna <jlpisa999@yahoo.com>
2023-08-20 18:37:13 -0400

    Added passive regeneration

--- Cogschan/Assets/Scripts/Entities/HealthTracker.cs
+++ Cogschan/Assets/Scripts/Entities/HealthTracker.cs
@@ -7,7 +7,14 @@ using UnityEngine.Events;
 /// </summary>
 public class HealthTracker : MonoBehaviour
 {
+    [Tooltip("The maximum amount of health this entity can have.")]
     [SerializeField] private int _maxHealth;
+    [Tooltip("The amount of time after last taking damage before this entity can begin regenerating health again.")]
+    [SerializeField] private float _regenResetDuration;
+    [Tooltip("The amount of time in between health regeneration ticks.")]
+    [SerializeField] private float _regenTickRate;
+    [Tooltip("The amount of health regenerated during a tick.")]
+    [SerializeField] private int _regenAmount;
 
     public UnityEvent OnHealthReset;
     /// <summary>
@@ -15,7 +22,7 @@ public class HealthTracker : MonoBehaviour
     /// </summary>
     public UnityEvent<float> OnDamaged;
     /// <summary>
-    /// An event that is triggered when this entity is healed.
+    /// An event that is triggered when this entity is healed by an outside source.
     /// </summary>
     public UnityEvent<float> OnHealed;
     /// <summary>
@@ -37,9 +44,21 @@ public class HealthTracker : MonoBehaviour
     /// </summary>
     public int MaxHealth => _maxHealth;
 
+    [SerializeField] private float _regenTimer = 0f;
+
     private void Awake()
     {
         ResetHealth();
+    }
+
+    private void Update()
+    {
+        _regenTimer -= Time.deltaTime;
+        if (_regenTimer <= 0f)
+        {
+            Heal(_regenAmount);
+            _regenTimer = _regenTickRate;
+        }
     }
 
     public void ResetHealth()
@@ -78,8 +97,11 @@ public class HealthTracker : MonoBehaviour
     public void Damage(int amount)
     {
         if (Health == 0) return;
+
         SetHealth(Health - amount);
+
         OnDamaged?.Invoke(amount);
+        _regenTimer = _regenResetDuration;
     }
 
     /// <summary>
