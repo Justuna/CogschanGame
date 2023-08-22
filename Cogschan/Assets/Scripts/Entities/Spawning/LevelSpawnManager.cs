c0fcd538ce798ecebd46475b62713b57abd3000b
kitkat-001 <54290919+kitkat-001@users.noreply.github.com>
2023-08-22 12:36:29 -0400

    clumps!

--- Cogschan/Assets/Scripts/Entities/Spawning/LevelSpawnManager.cs
+++ Cogschan/Assets/Scripts/Entities/Spawning/LevelSpawnManager.cs
@@ -25,11 +25,40 @@ public class LevelSpawnManager : SpawnManager
     [Tooltip("The maximum z-value of the randomized position.")]
     private float _maxZ;
 
-    protected override Vector3 GetSpawnLocation => ContinuousDistributions.GetRandomPointInBox(
-        _minX + transform.position.x,
-        _maxX + transform.position.x,
-        _minY + transform.position.y,
-        _maxY + transform.position.y,
-        _minZ + transform.position.z,
-        _maxZ + transform.position.z);
+    [Header("Enemy group parameters")]
+    [SerializeField]
+    [Tooltip("The minimum amount of enemies spawned in a group.")]
+    private int _minGroupSize;
+    [SerializeField]
+    [Tooltip("The maximum amount of enemies spawned in a group.")]
+    private int _maxGroupSize;
+    [SerializeField]
+    [Tooltip("The distance from the group center that enemise can spawn.")]
+    private float _radius;
+
+    private int _groupSize;
+    private int _spawnCount = 0;
+    private Vector3 _groupLocation;
+
+    protected override Vector3 GetSpawnLocation
+    {
+        get
+        {
+            if (_spawnCount == 0)
+            {
+                _groupSize = Random.Range(_minGroupSize, _maxGroupSize + 1);
+                _groupLocation = ContinuousDistributions.GetRandomPointInBox(
+                    _minX + transform.position.x,
+                    _maxX + transform.position.x,
+                    _minY + transform.position.y,
+                    _maxY + transform.position.y,
+                    _minZ + transform.position.z,
+                    _maxZ + transform.position.z);
+            }
+            _spawnCount++;
+            if (_spawnCount >= _groupSize)
+                _spawnCount = 0;
+            return ContinuousDistributions.GetRandomPointInAnnulus(0, _radius, _groupLocation);
+        }
+    }
 }
